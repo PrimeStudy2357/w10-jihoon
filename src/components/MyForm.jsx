0a7991ab94b23de1ca7da69e51210900690de8a1
anteater@mac <anteater1056@gmail.com>
2024-04-06 16:18:00 +0900

    실습내용 추가

--- src/components/MyForm.jsx
+++ src/components/MyForm.jsx
@@ -1,6 +1,7 @@
 function MyForm() {
   return (
-    <form action="https://randomuser.me/api/" method="get">
+    <form action="https://randomuser.me/api" method="get">
+      <input></input>
       <button type="submit">1. Submit Form</button>
     </form>
   );
